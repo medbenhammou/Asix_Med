d57c499ab1f6bf0cd0080249be9d97ad59445a3b
Mohamed <benhammou1012@gmail.com>
2024-02-13 16:17:38 +0100

    modifigando codigo

--- Ejmail.py
+++ Ejmail.py
@@ -1,42 +1,35 @@
-#Mohamed Ben Hammou
-
 from flask import Flask, request, render_template, redirect, url_for
-import mail_dict  #importem com un mòdul el codi d'acces al diccionari
+import mail_dict
+
 app = Flask(__name__)
 
-#URL arrel, si accedeixen a l'arrel, fem redirect a getmail
 @app.route("/")
 def inici():
     return redirect(url_for('getmail'))
-   
-#URL getmail, mostra formulari per introduir el nom 
-# quan omplim el formulari, mostra en una nova pàgina el resultat: el mail si l'ha trobat al diccionari o
-# un missatge d'error quan no el troba.
-# Permet enllaçar a la URL addmail
-@app.route('/getmail',methods = ['POST', 'GET'])
-def getmail():
-   if request.method == 'POST':
-      nom = request.form['nom']
-      nom = nom.capitalize() #en majúscules la primera lletra
-      correu = mail_dict.getmaildict(nom)
-      return render_template('resultgetmail.html',nom=nom,correu=correu)
-   else:
-      return render_template('formgetmail.html')
 
-#URL addmail, mostra formulari per introduir el nom i el correu 
-# quan omplim el formulari, mostra en una nova pàgina el resultat: si ha afegit el nom/correu al diccionari o
-# un missatge d'error quan ja existeix.
-# Permet enllaçar a la URL getmail  
-@app.route('/addmail',methods = ['POST', 'GET'])
+@app.route('/getmail', methods=['POST', 'GET'])
+def getmail():
+    if request.method == 'POST':
+        nom = request.form['nom']
+        nom = nom.capitalize()
+        correu = mail_dict.getmaildict(nom)
+        return render_template('resultgetmail.html', nom=nom, correu=correu)
+    else:
+        return render_template('formgetmail.html')
+
+@app.route('/addmail', methods=['POST', 'GET'])
 def addmail():
-   if request.method == 'POST':
-      modif=False
-      nom = request.form['nom']  #ull! si no ve, això acaba amb error
-      nom=nom.capitalize()
-      correu = request.form['correu']
-      if 'modif' in request.form: #el checkbox és opcional 
-         modif = True
-      result_msg = mail_dict.addmaildict(nom, correu, modif)
-      return render_template('resultaddmail.html',nom = nom, correu=correu, result_msg = result_msg)
-   else:
-      return render_template('formaddmail.html')
+    if request.method == 'POST':
+        modif = False
+        nom = request.form['nom']
+        nom = nom.capitalize()
+        correu = request.form['correu']
+        if 'modif' in request.form:
+            modif = True
+        result_msg = mail_dict.addmaildict(nom, correu, modif)
+        return render_template('resultaddmail.html', nom=nom, correu=correu, result_msg=result_msg)
+    else:
+        return render_template('formaddmail.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ mail_dict.py
@@ -0,0 +1,45 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="mohamed",
+    password="asixstudent",
+    database="my_base_datos"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM nombre_email")
+myresult = mycursor.fetchall()
+
+NOTROBAT = "NOTROBAT"
+AFEGIT = "AFEGIT"
+MODIFICAT = "MODIFICAT"
+JAEXISTEIX = "JAEXISTEIX"
+
+def getmaildict(nom):
+    for x in myresult:
+        if nom == x[0]:
+            email = x[1]
+            return email
+    return NOTROBAT
+
+def addmaildict(nom, correu, modif=False):
+    oldcorreu = getmaildict(nom)
+    if oldcorreu == NOTROBAT:
+        sql = "INSERT INTO nombre_email (nombre, email) VALUES (%s, %s)"
+        val = (nom, correu)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return AFEGIT
+
+    elif oldcorreu != correu and modif:
+        sql = "UPDATE nombre_email SET email = %s WHERE nombre = %s"
+        val = (correu, nom)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return MODIFICAT
+
+    return JAEXISTEIX
